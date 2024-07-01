1e65d0cf6aa5bdf428d2e9b30c19f619504e0f38
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-07-01 19:29:31 +0200

    Premier commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require ('dotenv').config();
+
+const express = require('express');
+const session = require('express-session'); 
+const cors = require('cors')
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const router = require('./app/routers/router');
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(express.static('assets'));
+app.use(cors('*'));
+
+app.use(express.json());
+app.use(router)
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
